dffd8736a7e83ab34d9eed155d09058236af85e1
nachocalafate <calafateignacio@gmail.com>
2026-06-27 02:57:17 -0300

    Update Product model and configure database properties

--- src/main/java/com/calafate/inventory_management/model/Product.java
+++ src/main/java/com/calafate/inventory_management/model/Product.java
@@ -1,7 +1,7 @@
 package com.calafate.inventory_management.model;
 
 import jakarta.persistence.*;
-        import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
